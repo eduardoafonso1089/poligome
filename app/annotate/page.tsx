f7d80e0e0bb1f8d5809a08724175151d4090fb39
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-13 12:46:24 -0300

    Use dedicated Demo tutorial presentation

--- app/annotate/page.tsx
+++ app/annotate/page.tsx
@@ -2,7 +2,8 @@ import { CanonicalEditorWorkbench } from "../editor/workbench/canonical-editor-w
 import styles from "./exact-pre-refactor-refinements.module.css";
 import final from "./exact-pre-refactor-final.module.css";
 import routing from "./functional-interaction-routing.module.css";
+import demo from "./demo-tutorial-parity.module.css";
 
 export default function AnnotatePage() {
-  return <div className={`${styles.contract} ${final.contract} ${routing.contract}`}><CanonicalEditorWorkbench /></div>;
+  return <div className={`${styles.contract} ${final.contract} ${routing.contract} ${demo.contract}`}><CanonicalEditorWorkbench /></div>;
 }
