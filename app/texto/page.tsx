c1687e2d2cae0313300b384401b54b296e43b6aa
Eduardo Afonso <eduardoafonso1089@gmail.com>
2026-09-20 17:14:04 -0300

    chore: remove obsolete texto redirect route

--- app/texto/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TextoRedirect() {
-  return <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
-    <meta httpEquiv="refresh" content="0;url=/annotate" />
-    <link rel="canonical" href="/annotate" />
-    <p>Esta rota está temporariamente indisponível. Abra <a href="/annotate">/annotate</a>.</p>
-  </main>;
-}
